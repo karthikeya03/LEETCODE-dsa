fc24fff1127abddd87773601c32efa14e76fdc0e
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-08-23 10:59:26 +0530

    Time: 11 ms (82.14%) | Memory: 19.6 MB (28.56%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int result = 0;
         for(int i = 0; i < nums.size(); ++i) {
-            bool foundDuplicate = false;
-            for(int j = 0; j < nums.size(); ++j) {
-                if(i != j && nums[i] == nums[j]) {
-                    foundDuplicate = true;
-                    break;
-                }
-            }
-            if(!foundDuplicate) {
-                return nums[i];
-            }
+            result ^= nums[i];  // XOR each element with the result
         }
-        return -1; // This line handles the case where no single number is found (though it's guaranteed that there is one single number in the problem description).
+        return result;
     }
 };
