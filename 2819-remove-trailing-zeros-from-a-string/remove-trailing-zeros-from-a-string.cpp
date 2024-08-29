82594c3f59a6ce8c8754e6dfc49af1f92eb2b57c
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-08-29 15:51:45 +0530

    Time: 12 ms (39.26%) | Memory: 10.8 MB (59.81%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.cpp
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeTrailingZeros(string num) {
-        size_t n=num.size();
+        int n=num.size();
         while (num.back() == '0') {
             num.replace(n-1,1,"");
             n--;
