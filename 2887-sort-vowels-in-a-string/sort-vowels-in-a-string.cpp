d2f7fcaf32625245262364dbc5ad1954550a4e6e
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-08-29 19:28:18 +0530

    Time: 33 ms (48.16%) | Memory: 14.8 MB (87.90%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -9,17 +9,14 @@ public:
         string chch;
         int n = s.size();
         
-        // Collect all vowels from the string
         for (int i = 0; i < n; ++i) {
             if (isvow(s[i])) {
                 chch.push_back(s[i]);
             }
         }
 
-        // Sort the collected vowels
         sort(chch.begin(), chch.end());
 
-        // Replace the vowels in the original string with sorted vowels
         for (int i = 0, j = 0; i < n; ++i) {
             if (isvow(s[i])) {
                 s[i] = chch[j++];
