8164faa874ed845a023d611879d8519587c0ff92
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-09-04 15:16:57 +0530

    Time: 64 ms (82.32%) | Memory: 37.4 MB (83.70%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_set<string> obs;
+        set<pair<int, int>> obs;
         for (auto& obstacle : obstacles) {
-            obs.insert(to_string(obstacle[0]) + "," + to_string(obstacle[1]));
+            obs.insert({obstacle[0], obstacle[1]});
         }
 
         int x = 0, y = 0;
-        int dx = 0, dy = 1; 
+        int dx = 0, dy = 1; // Initial direction is "up" (North)
         int ans = 0;
 
-        for (int c : commands) 
-        {
-            if (c == -1)
-             { 
+        for (int c : commands) {
+            if (c == -1) { // Right turn
                 int temp = dx;
                 dx = dy;
                 dy = -temp;
-            } else if (c == -2)
-             { 
+            } else if (c == -2) { // Left turn
                 int temp = dx;
                 dx = -dy;
                 dy = temp;
-            } else 
-            { 
-                for (int i = 0; i < c; ++i) 
-                {
+            } else { // Move forward
+                for (int i = 0; i < c; ++i) {
                     int nx = x + dx;
                     int ny = y + dy;
-                    if (obs.find(to_string(nx) + "," + to_string(ny)) != obs.end()) 
-                    {
-                        break; 
+                    if (obs.find({nx, ny}) != obs.end()) {
+                        break; // Stop if there's an obstacle
                     }
                     x = nx;
                     y = ny;
-                    ans = max(ans, x * x + y * y); 
+                    ans = max(ans, x * x + y * y);
                 }
             }
         }
