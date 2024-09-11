b19340e939f54dd4ea073eff3d3489a88dd22c41
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-09-11 20:58:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (44.87%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int bit = start ^ goal;  
+        int differingBits = start ^ goal;
         int count = 0;
-        while (bit) {
-            count += (bit & 1);  // Add the least significant bit to count
-            bit >>= 1;           // Shift right by 1 to process the next bit
+        while (differingBits) {
+            count += (differingBits & 1);
+            differingBits >>= 1;
         }
         return count;
     }
