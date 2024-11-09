5011ead6a003ba05984fd7c7be410bc7b5770728
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-11-09 11:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (60.20%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,13 +5,15 @@ public:
         if (strs.empty()) 
         return "";  
         string pre = strs[0];  
-        for (int i=1;i<n;++i) {  
-            while (strs[i].find(pre)!=0) {  
+        for (int i=1;i<n;++i) 
+        {  
+            while (strs[i].find(pre)!=0) 
+            {   
                 pre=pre.substr(0, pre.size()-1);  
                 if (pre.empty()) 
                 return "";  
             }  
         }  
-        return pre;  
+        return pre;
     }  
 };
