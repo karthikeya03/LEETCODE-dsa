1a31bc08d23732529248c8c585854b28ab15c339
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-01-25 00:28:31 +0530

    Time: 161 ms (88.36%) | Memory: 105.6 MB (5.17%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -5,7 +5,7 @@ public:
         int i=0; int j=n-1;
         sort(nums.begin(),nums.end());
         vector<int>result;
-        while(i<j)
+        while(i<=j)
         {
             result.push_back(nums[i]+nums[j]);
             j--;
