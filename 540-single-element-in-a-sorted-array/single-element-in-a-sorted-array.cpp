398426b44733ff68d63d5204a996e679f2bf3e41
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-06-06 20:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.59%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,18 +4,16 @@ public:
         int count = 0;
         int ans = -1;
 
-        for (int i = 0; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size(); i+=2) {
             if (i < nums.size() - 1 && nums[i] == nums[i + 1]) {
                 ++count;
                 if (count == 1) {
                     ans = i;
                 }
-                ++i; // skip the next one because it's a pair
             } else {
-                return nums[i]; // if no pair found, this is the single element
+                return nums[i]; 
             }
         }
-
-        return nums[nums.size() - 1]; // fallback return
+        return -1;
     }
 };
