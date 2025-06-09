a893e3891731626adb921df62db8086bb7f7354a
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-06-09 16:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.04%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n = s.length();
-        if (n != goal.length()) {
-            return false;
+        int n=s.size();
+        if(n!=goal.size())
+        return false;
+        for(int i=0;i<n;++i){
+             reverse(s.begin(), s.begin() + (n - 1));
+            reverse(s.begin() + (n - 1), s.end());
+            reverse(s.begin(), s.end());
+
+            if(s==goal)
+            return true;
         }
-        
-        for (int i = 0; i < n; ++i) {
-            string rotated = s.substr(1) + s[0];
-            s = rotated;
-            if (s == goal) {
-                return true;
-            }
-        }
-        
         return false;
     }
 };
