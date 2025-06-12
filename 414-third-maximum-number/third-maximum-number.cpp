820ec0eacbb1f027726e8061d5e3ae5380906c4c
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-06-12 22:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (56.71%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+        long long first = LLONG_MIN, second = LLONG_MIN, third = LLONG_MIN;
 
-        for (int num : nums) {
-            if (num == first || num == second || num == third) continue;
+        for(int num : nums) {
+            if(num == first || num == second || num == third) continue;
 
-            if (num > first) {
+            if(num > first) {
                 third = second;
                 second = first;
                 first = num;
-            } else if (num > second && num<first) {
+            } else if(num > second) {
                 third = second;
                 second = num;
-            } else if (num > third && num<second && num<first) {
+            } else if(num > third) {
                 third = num;
             }
         }
 
-        return third==LONG_MIN ? first : third;
+        return (third == LLONG_MIN) ? first : third;
     }
 };
