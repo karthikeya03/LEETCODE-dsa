28da70f491c618cc8665c13429468689f61192f4
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-06-16 07:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.21%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,15 +3,6 @@
 class Solution {
 public:
     void sortColors(std::vector<int>& nums) {
-        int temp;
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < nums.size() - i - 1; ++j) {
-                if (nums[j] > nums[j + 1]) {
-                    temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
-        }
+        sort(nums.begin(),nums.end());
     }
 };
