0b85fb7cf63f8a46eb4ccc1dcf1917f8dd5a72ef
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-07-02 21:02:36 +0530

    Time: 11 ms (44.91%) | Memory: 8.3 MB (40.72%) - LeetSync

--- 1844-maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.cpp
+++ 1844-maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int countBalls(int lowLimit, int highLimit) {
         unordered_map<int,int>mpp;
-        for(int i=lowLimit;i<=highLimit;i++)
-        {
-            int sum=0;
+        for(int i=lowLimit;i<=highLimit;++i){
             int n=i;
+            int sum=0;
             while(n){
                 sum+=n%10;
                 n=n/10;
@@ -13,9 +12,9 @@ public:
             mpp[sum]++;
         }
         int maxx=0;
-        for(auto num:mpp){
-            if(maxx<num.second)
-            maxx=num.second;
+        for(auto it:mpp){
+            if(it.second>maxx)
+            maxx=it.second;
         }
         return maxx;
     }
