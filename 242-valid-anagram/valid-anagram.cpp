e3b1627117084380d06f529c16e197144a83b7d6
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-07-03 15:17:03 +0530

    Time: 7 ms (21.12%) | Memory: 9.7 MB (74.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
-    bool isAnagram(std::string s, std::string t) {
-    if (s.length() != t.length()) {
-        return false;
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
-    std::sort(s.begin(), s.end());
-    std::sort(t.begin(), t.end());
-    
-    return s==t;
-}
 };
