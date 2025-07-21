e76ec9817683797dfb97c28599676bd6f6ca07b6
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-07-21 23:38:03 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (98.59%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(char ch:stones){
+            if(jewels.find(ch)!=string::npos)
+            count++;
+        }
+        return count;
+    }
+};
