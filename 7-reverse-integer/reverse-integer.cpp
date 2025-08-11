f25a3ea2b7274c01ab36c89b385fdb651c34584a
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-08-12 00:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.33%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -4,10 +4,11 @@ public:
         int n=x;long long rev=0;
         while(n!=0){
             int digit=n%10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7)) return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8)) return 0;
             rev=rev*10+digit;
             n=n/10;
         }
-        if(rev>INT_MAX || rev<INT_MIN) return 0;
         return (int)rev;
     }
 };
