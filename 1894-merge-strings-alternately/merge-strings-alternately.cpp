26d7a83ee36d4a67a24905c14e2e57b5d386251d
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-08-12 15:22:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.96%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,19 +1,14 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result;  
-        int n = word1.size(), m = word2.size();
-        int i = 0;
-        
-        while (i < n || i < m) {
-            if (i < n) result += word1[i]; 
-            if (i < m) result += word2[i];  
-            i++;
+        int i=0,j=0;
+        string neww="";
+        while(i<word1.size()||j<word2.size()){
+            if(i<word1.size())
+            neww+=word1[i++];
+            if(j<word2.size())
+            neww+=word2[j++];
         }
-        return result; 
+        return neww;
     }
 };
