8cc07821f3c23ee6016d6ef867d705e8c79bf46b
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-08-13 10:17:59 +0530

    Time: 49 ms (57.17%) | Memory: 29.1 MB (48.68%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -20,8 +20,6 @@ public:
                     j++; k--;
                     while(j<k && nums[j]==nums[j-1])
                         j++;
-                    while(j<k && nums[k]==nums[k+1])
-                        k--;
                 }
             }
         }
