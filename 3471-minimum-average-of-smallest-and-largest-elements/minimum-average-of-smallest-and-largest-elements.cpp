c02b5d53b8858b7cf10c4fcd7cbd65473cabb7b8
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-08-15 09:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (65.01%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     double minimumAverage(vector<int>& nums) {
         int n=nums.size();
-        vector<double>result;
+        vector<double>res;
         int t=n/2;
         while(t--){
-        int minn=*min_element(nums.begin(),nums.end());
-        int maxx=*max_element(nums.begin(),nums.end());
-                double avg=(minn+maxx)/2.0;
-                result.push_back(avg);
-        auto it=find(nums.begin(),nums.end(),minn);
-        if(it!=nums.end()) nums.erase(it);
-        auto itt=find(nums.begin(),nums.end(),maxx);
-        if(itt!=nums.end()) nums.erase(itt);
+            auto maxt=max_element(nums.begin(),nums.end());
+            double maxx=*maxt;
+            nums.erase(maxt);
+            auto mint=min_element(nums.begin(),nums.end());
+            double minn=*mint;
+            nums.erase(mint);
+            double avg=(double)((maxx+minn)/2);
+            res.push_back(avg);
         }
-        return *min_element(result.begin(),result.end());
+        return *min_element(res.begin(),res.end());
     }
 };
