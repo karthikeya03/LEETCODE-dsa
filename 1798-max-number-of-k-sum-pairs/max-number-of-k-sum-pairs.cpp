dedc672fbe0ed368de79cfc6494f2b23bf77b3ea
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-08-15 16:38:04 +0530

    Time: 127 ms (15.36%) | Memory: 76.7 MB (23.91%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int maxOperations(vector<int>& nums, int target) {
-        int count=0; 
-        sort(nums.begin(),nums.end());
-        int i=0,j=nums.size()-1;
-        while(i<j){
-            int sum=nums[i]+nums[j];
-            if(sum>target)
-            {j--;}
-            else if(sum<target){
-                i++;
-            }
-            else{
-                i++; j--;
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int count = 0;
+
+        for (int num : nums) {
+            int target = k - num;
+            if (freq[target] > 0) { // found a complement
                 count++;
+                freq[target]--; // use one occurrence of the complement
+            } else {
+                freq[num]++; // store current number for future pairs
             }
         }
+
         return count;
     }
 };
