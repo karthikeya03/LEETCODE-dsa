f52f52a33dbb955304169da109fe651a3ae5f83f
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2025-10-12 20:30:00 +0530

    Time: 6 ms (25.06%) | Memory: 9.9 MB (25.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())
+        if(s.size()!=t.size())
         return false;
-        unordered_map<char,int>mpp;
-        for(char ch:s)
-            mpp[ch]++;
-            for(char ch:t){
-                mpp[ch]--;
-                if(mpp[ch]<0)
-                return false;
-            }
-        return true;
+       unordered_map<char,int>mpp;
+       for(char ch:s)
+        mpp[ch]++;
+       
+       for(char chh:t)
+       mpp[chh]--;
+
+       for(auto it:mpp){
+        if(it.second!=0)
+        return false;
+       }
+       return true;
+       
     }
 };
