b2eb86e6dcb925df061a3ab8da0d4674285e7038
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2026-01-14 12:44:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (14.57%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,24 +1,17 @@
-#include <vector>
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> result;  
-        
-        for (int i = 1; i <= n; ++i) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                result.push_back("FizzBuzz");
-            } else if (i % 3 == 0) {
-                result.push_back("Fizz");
-            } else if (i % 5 == 0) {
-                result.push_back("Buzz");
-            } else {
-                result.push_back(to_string(i));  
-            }
+        vector<string>res;
+        for(int i=1;i<=n;++i){
+            if(i%3==0 && i%5==0)
+            res.push_back("FizzBuzz");
+            else if(i%3==0)
+            res.push_back("Fizz");
+            else if(i%5==0)
+            res.push_back("Buzz");
+            else
+            res.push_back(to_string(i));
         }
-        return result;
+        return res;
     }
 };
