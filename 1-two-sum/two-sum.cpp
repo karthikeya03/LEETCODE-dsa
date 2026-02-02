403ccc04902d435e0bfb5f82eba0d9bce1d8fbdf
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2026-02-02 11:26:08 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (36.34%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();++i){
-            int ans=target-nums[i];
-            if(mpp.count(ans))
-            return {mpp[ans],i};
+            int need=target-nums[i];
+            if(mpp.count(need))
+            return {mpp[need],i};
             mpp[nums[i]]=i;
         }
         return {};
